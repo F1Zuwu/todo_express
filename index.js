56d3b63fa6e593f3575994811a7ccb6ae28ddd58
F1Zuwu <113283931+F1Zuwu@users.noreply.github.com>
2024-09-09 11:49:33 +0300

    5. poolik

--- index.js
+++ index.js
@@ -1,13 +1,70 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const fs = require('fs')
 const port = 8080
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
+app.use(express.urlencoded({extended: true}))
+
+const readFile = (filename) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filename, "utf8", (err, data) => {
+            if (err) {
+                console.error(err)
+                return;
+            }
+
+            console.log(data)
+
+            const tasks = JSON.parse(data)
+            resolve(tasks)
+        })
+    })
+}
 
 app.get('/', (req, res) => {
-    res.render('index')
+    readFile("./tasks.json")
+        .then(tasks => {
+            res.render('index', { tasks: tasks })
+        })
+})
+
+app.post("/", (req, res) => {
+    readFile("./tasks.json")
+        .then(tasks => {
+
+            console.log(tasks[tasks.lenght - 1])
+
+            let index = null
+
+            if (tasks.lenght === 0) {
+                index = 0
+            } else {
+                index = tasks[tasks.lenght - 1].id + 1;
+            }
+
+            const newTask = {
+                "id" : index,
+                "task": req.body.tasks
+            }
+
+            console.log(newTask)
+
+            tasks.push(newTask)
+            const data = JSON.stringify(tasks, null, 2)
+            fs.writeFile('./tasks.json', data, err => {
+                if (err) {
+                    console.log(err)
+                    return;
+                } else {
+                    console.log("saved")
+                }
+
+                res.redirect('/')
+            })
+        })
 })
 
 app.listen(port, () => {
